2de9c085fef74573099302935e8598c67a4b5989
Okash Khawaja <okash.khawaja@gmail.com>
2014-01-25 10:14:44 +0000

    added BFS which also returns shortest paths from starting node.

--- src/Graphs.Library/GeneralGraphProblems.cs
+++ src/Graphs.Library/GeneralGraphProblems.cs
@@ -58,5 +58,41 @@ namespace Graphs.Library
                 }
             }
         }
+
+        public static int[] PerformBfsAndReturnShortestPaths(Graph g, Action<Node> processNode, int startingNode = 0)
+        {
+            if (g.Count == 0)
+            {
+                throw new ArgumentException("Graph is empty.");
+            }
+
+            int[] paths = new int[g.Capacity];
+            foreach (int p in paths)
+            {
+                paths[p] = -1;
+            }
+            Queue<Node> q = new Queue<Node>();
+            Node n = g[startingNode];
+            n.Visited = true;
+            q.Enqueue(n);
+            paths[startingNode] = startingNode;
+
+            while (q.Count > 0)
+            {
+                n = q.Dequeue();
+                processNode(n);
+                foreach (Edge e in n.Edges)
+                {
+                    if (!g[e.ToIndex].Visited)
+                    {
+                        g[e.ToIndex].Visited = true;
+                        q.Enqueue(g[e.ToIndex]);
+                        paths[e.ToIndex] = n.Index;
+                    }
+                }
+            }
+
+            return paths;
+        }
     }
 }
