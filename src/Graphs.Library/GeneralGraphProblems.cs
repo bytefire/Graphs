beaa9af6e77c1eafa1566c734212b92cb169e34a
Okash Khawaja <okash.khawaja@gmail.com>
2014-01-25 10:57:32 +0000

    added method to find shortest path from among a set of starting nodes.

--- src/Graphs.Library/GeneralGraphProblems.cs
+++ src/Graphs.Library/GeneralGraphProblems.cs
@@ -94,5 +94,59 @@ namespace Graphs.Library
 
             return paths;
         }
+
+        /// <summary>
+        /// Searches the shortest path from any of the starting nodes to a node whose data is equal to the searched value.
+        /// </summary>
+        /// <param name="g">Graph to search</param>
+        /// <param name="startingNodes">The nodes from which to start.</param>
+        /// <param name="searchedValue">The value being searched.</param>
+        /// <returns>Path to a node containing that value. This will be null if the value is not found.</returns>
+        public static int[] MultipleShortestPath(Graph g, int[] startingNodes, int searchedValue)
+        {
+            if (g.Count == 0)
+            {
+                throw new ArgumentException("Graph is empty.");
+            }
+
+            int[] paths = new int[g.Capacity];
+            foreach (int p in paths)
+            {
+                paths[p] = -1;
+            }
+            Queue<Node> q = new Queue<Node>();
+            Node n;
+            foreach (int i in startingNodes)
+            {
+                n = g[i];
+                n.Visited = true;
+                q.Enqueue(n);
+                paths[i] = i;
+                if (n.Data == searchedValue)
+                {
+                    return paths;
+                }
+            }
+
+            while (q.Count > 0)
+            {
+                n = q.Dequeue();
+                foreach (Edge e in n.Edges)
+                {
+                    if (!g[e.ToIndex].Visited)
+                    {
+                        g[e.ToIndex].Visited = true;
+                        q.Enqueue(g[e.ToIndex]);
+                        paths[e.ToIndex] = n.Index;
+                        if (g[e.ToIndex].Data == searchedValue)
+                        {
+                            return paths;
+                        }
+                    }
+                }
+            }
+
+            return null;
+        }
     }
 }
