88fa9a843abfcbbb4dd508bbb1356451b9be8cae
Okash Khawaja <okash.khawaja@gmail.com>
2014-02-01 11:06:35 +0000

    Added BubbleUp method to PriorityQueue and made its index zero-based.

--- src/Graphs.Utilities/PriorityQueue.cs
+++ src/Graphs.Utilities/PriorityQueue.cs
@@ -10,9 +10,14 @@ namespace Graphs.Utilities
     /// A simple implementation of minimum priority queue using binary heap. 
     /// On reaching capacity, the priority doesn't resize. Instead it throws overflow
     /// exception.
-    /// This is based on the book The Algorithm Design Manual by Steven Skiena.
     /// </summary>
     /// <typeparam name="T">Type of elements. Elements must be comparable to eachother.</typeparam>
+    /// <remarks>
+    /// This is based on the book The Algorithm Design Manual by Steven Skiena. Following changes have
+    /// been made from the original:
+    /// 1. Array indices start at zero rather than one.
+    /// 2. Recursive methods have been made iterative.
+    /// </remarks>
     public class PriorityQueue<T> where T : IComparable<T>
     {
         T[] _queue;
@@ -45,18 +50,51 @@ namespace Graphs.Utilities
             _queue = new T[capacity];
         }
 
+        public void Insert(T item)
+        {
+            if (Count == Capacity)
+            {
+                throw new OverflowException("The priority queue has reached its capacity. The item cannot be inserted.");
+            }
+            Count++;
+            _queue[Count - 1] = item;
+            BubbleUp(Count - 1);
+        }
+
+        private void BubbleUp(int p)
+        {
+            // if root of the heap, i.e. no parent then return straightaway
+            if (Parent(p) == -1)
+            {
+                return;
+            }
+
+            while (_queue[Parent(p)].CompareTo( _queue[p])>0)
+            {
+                SwapElementsAt(p, Parent(p));
+                p = Parent(p);
+            }
+        }
+
+        private void SwapElementsAt(int index1, int index2)
+        {
+            T temp = _queue[index2];
+            _queue[index2] = _queue[index1];
+            _queue[index1] = temp;
+        }
+
 
         private int Parent(int n)
         {
-            if (n == 1)
+            if (n == 0)
             {
                 return -1;
             }
-            return n / 2;
+            return (n - 1) / 2;
         }
         private int YoungChild(int n)
         {
-            return 2 * n;
+            return 2 * n + 1;
         }
     }
 }
